aef2ae2abbd8830652f294805ac945dc5175fdf6
username <atcgsp912@gmail.com>
2021-02-04 18:18:34 +0900

    2021.02.04
    가짜객체 만들기
    Test code
    
    MockBean
    Mockito framework 적용
    given 사용하여, Test code의 객체를 초기화 후, 값 부여.
    
    SpyBean 주석처리

--- eatgo-api/src/test/java/kr/co/spring/lec/eatgo/application/RestaurantServiceTest.java
+++ eatgo-api/src/test/java/kr/co/spring/lec/eatgo/application/RestaurantServiceTest.java
@@ -1,31 +1,72 @@
 package kr.co.spring.lec.eatgo.application;
 
-import kr.co.spring.lec.eatgo.domain.*;
+import kr.co.spring.lec.eatgo.domain.MenuItem;
+import kr.co.spring.lec.eatgo.domain.MenuItemRepository;
+import kr.co.spring.lec.eatgo.domain.Restaurant;
+import kr.co.spring.lec.eatgo.domain.RestaurantRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 
 public class RestaurantServiceTest {
 
     private RestaurantService restaurantService;
 
+    @Mock
     private RestaurantRepository restaurantRepository;
 
+    @Mock
     private MenuItemRepository menuItemRepository;
+
+    // Mock 로 변경
+//    private RestaurantService restaurantService;
+//
+//    private RestaurantRepository restaurantRepository;
+//
+//    private MenuItemRepository menuItemRepository;
 
     @Before // test 를 실행하기 전에 반드시 한번씩 실행하게 된다.
     public void setUp(){
-        restaurantRepository = new RestaurantRepositoryImpl();
-        menuItemRepository = new MenuItemRepositoryImpl();
+//        restaurantRepository = new RestaurantRepositoryImpl();
+//        menuItemRepository = new MenuItemRepositoryImpl();
+
+        //Mock 개체 설정
+        MockitoAnnotations.initMocks(this);
+
+        mockRestaurantRepository();
+        mockMenuItemRepository();
+
         restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+    }
+
+    private void mockRestaurantRepository() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant
+                = new Restaurant("Bob zip", "Seoul", 1004L);
+        restaurants.add(restaurant);
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+    }
+
+    private void mockMenuItemRepository() {
+
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Kimchi"));
+        given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
     }
 
     @Test
     public void getRestaurants(){
+
         List<Restaurant> restaurants = restaurantService.getRestaurants();
 
         assertThat(restaurants.get(0).getid(), is(1004L));
--- eatgo-api/src/test/java/kr/co/spring/lec/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/spring/lec/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,19 +1,21 @@
 package kr.co.spring.lec.eatgo.interfaces;
 
 import kr.co.spring.lec.eatgo.application.RestaurantService;
-import kr.co.spring.lec.eatgo.domain.MenuItemRepository;
-import kr.co.spring.lec.eatgo.domain.MenuItemRepositoryImpl;
-import kr.co.spring.lec.eatgo.domain.RestaurantRepository;
-import kr.co.spring.lec.eatgo.domain.RestaurantRepositoryImpl;
+import kr.co.spring.lec.eatgo.domain.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,17 +27,29 @@ public class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc; // Controller Test
 
-    @SpyBean// Controller 에 원하는 객체를 주입할 수 있다.
-    private RestaurantService restaurantService;
+    @MockBean
+    RestaurantService restaurantService;
 
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepository restaurantRepository;
 
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
+    // 나머지 객체도 Mock Object 로 대체
+    // 실제 RestaurantRepository, MenuItemRepository 를 restaurantService 받는 것 처럼 움직이는 MockBean 으로 대체
+//    @SpyBean// Controller 에 원하는 객체를 주입할 수 있다.
+//    private RestaurantService restaurantService;
+//
+//    @SpyBean(RestaurantRepositoryImpl.class)
+//    private RestaurantRepository restaurantRepository;
+//
+//    @SpyBean(MenuItemRepositoryImpl.class)
+//    private MenuItemRepository menuItemRepository;
 
     @Test
     public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant("Bob zip", "Seoul" , 1004L));
+
+        // Mockito framework 으로 만든 가짜 객체
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk()) // Http Status : 200(Ok)
                 .andExpect(content().string(containsString("\"name\":\"Bob zip\""))) // 포함 값 중에 ("name":"Bob zip")포함
@@ -45,6 +59,14 @@ public class RestaurantControllerTest {
 
     @Test
     public void detail() throws Exception{
+        Restaurant restaurant1 = new Restaurant("Bob zip", "Seoul" , 1004L);
+        Restaurant restaurant2 = new Restaurant("Cyber Food", "Seoul" , 2020L);
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+        restaurant2.addMenuItem(new MenuItem("Kimchi"));
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
         mvc.perform(get("/restaurants/1004")) // id : 1004 일때 Test
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"name\":\"Bob zip\""))) // 포함 값 중에 ("name":"Bob zip")포함
